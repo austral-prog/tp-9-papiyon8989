446f5c8ff4b9f22dba5f8e0bcbfbb44529e3c787
papiyon8989 <162721612+papiyon8989@users.noreply.github.com>
2024-06-07 04:33:22 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,20 +1,39 @@
-def create_inventory(items):inventory = {}
+
+def create_inventory(items):
+    inventory = dict()
+    for item in items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1 
+    return inventory
+
+
+
+def add_items(inventory, items):
+
     for item in items:
         if item in inventory:
             inventory[item] += 1
         else:
             inventory[item] = 1
-    return inventorydef add_items(inventory, items):for item in items:
-        if item in inventory:
-            inventory[item] += 1
-        else:
-            inventory[item] = 1
-    return inventorydef decrement_items(inventory, items):for item in items:
+    return inventory
+
+
+def decrement_items(inventory, items):
+
+    for item in items:
         if item in inventory and inventory[item] > 0:
             inventory[item] -= 1
-            if inventory[item] < 0:
-                inventory[item] = 0
-    return inventorydef remove_item(inventory, item):if item in inventory:
-        del inventory[item]
-    return inventoryef list_inventory(inventory):return [(item, quantity) for item, quantity in inventory.items() if quantity > 0]
+    return inventory
 
+
+def remove_item(inventory, item):
+
+    if item in inventory:
+        del inventory[item]
+    return inventory
+
+def list_inventory(inventory):
+
+    return [(item, quantity) for item, quantity in inventory.items() if quantity > 0]
