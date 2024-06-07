74916cd5423e6b2d1b46f3d5a814c1be2b0384fa
papiyon8989 <162721612+papiyon8989@users.noreply.github.com>
2024-06-07 04:26:54 -0300

    Update tp9_dicts_test.py

--- tp9_dicts_test.py
+++ tp9_dicts_test.py
@@ -1,37 +1,42 @@
-import unittest
-from dicts import create_inventory, add_items, decrement_items, remove_item, list_inventory
+
+def create_inventory(items):
+    inventory = dict()
+    for item in items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1 
+    return inventory
 
 
-class InventoryTest(unittest.TestCase):
-    
-    def test_create_inventory(items):
-        
-    inventory = {}
+
+def add_items(inventory, items):
+
     for item in items:
         if item in inventory:
             inventory[item] += 1
         else:
             inventory[item] = 1
     return inventory
-    
-    def test_add_items(inventory, items):
-    for item in items:
-        if item in inventory:
-            inventory[item] += 1
-        else:
-            inventory[item] = 1
-    return inventory
-    
-    def test_decrement_items(inventory, items):
+
+
+def decrement_items(inventory, items):
+
     for item in items:
         if item in inventory and inventory[item] > 0:
             inventory[item] -= 1
     return inventory
-    
-    def test_remove_item(inventory, item):
+
+
+def remove_item(inventory, item):
+
     if item in inventory:
         del inventory[item]
-    return inventorydef list_inventory(inventory):
+    return inventory
+
+def list_inventory(inventory):
+
     return [(item, quantity) for item, quantity in inventory.items() if quantity > 0]
+
     
    
